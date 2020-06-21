19b76533a998e5b4ee9b8f2a7491f40974a1040d
Jose <leojjosepf@gmail.com>
2020-06-21 02:11:05 -0500

    Variable de entorno oculta

--- server/config/config.js
+++ server/config/config.js
@@ -9,6 +9,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://admin:NK8q7mfqqNUa5KNY@cluster0-nkzju.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.urlDB = urlDB;
